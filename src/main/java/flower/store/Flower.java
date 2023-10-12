8f3a4922bc75f9a86742ae107ec54eb1f4665755
Viktoriya264721 <stetsyshyn.pn@ucu.edu.ua>
2023-10-12 21:50:03 +0300

    Viktoriya

--- src/main/java/flower/store/Flower.java
+++ src/main/java/flower/store/Flower.java
@@ -1,10 +1,13 @@
 package flower.store;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
-
-
+@NoArgsConstructor
+@AllArgsConstructor
 @Setter
 public class Flower {
     @Getter
@@ -14,6 +17,13 @@ public class Flower {
     private double price;
     @Getter
     private FlowerType flowerType;
+
+    public Flower(Flower flower){
+        this.flowerType = flower.flowerType;
+        this.price = flower.price;
+        this.sepalLength = flower.sepalLength;
+        this.color = flower.color;
+    }
 
     public String getColor() {
         return color.toString();
---
+++ src/main/java/flower/store/Store.java
@@ -0,0 +1,10 @@
+package flower.store;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import flower.filters.SearchFilter;
+
+public class Store{
+
+}
